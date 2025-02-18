b1082e49746d0d45999a93a5b7a8903ad520ec27
Лев Романович Панас <jieba2005@list.ru>
2025-02-18 17:08:17 +0300

    add ignore, week(program)

---
+++ week.py
@@ -0,0 +1,19 @@
+import locale
+from datetime import date, timedelta, datetime
+
+locale.setlocale(locale.LC_TIME, "ru_RU.UTF-8")
+d, m, y = [int(i) for i in input("Введите дату в формате дд,мм,гггг: ").split(".")]
+dt = date(y, m, d)
+
+print("\n" f"Введена дата: {datetime.strftime(dt, '%x')}")
+print(f"День недели: {datetime.strftime(dt, '%A')}")
+print("\n" "Неделя, в которой есть данная дата: ")
+
+while dt.weekday() != 0:
+    dt -= timedelta(days=1)
+
+for i in range(7):
+    print(datetime.strftime(dt, "%d %B %Y - %A"))
+    dt += timedelta(days=1)
+    if dt.weekday() == 0:
+        break
