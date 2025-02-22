a184434b13e1ff71ff700a9a7bddade04445fc7e
Лев Романович Панас <jieba2005@list.ru>
2025-02-22 20:23:28 +0300

    3.6_2 ver 1

--- 3,6_2_func-get_the_fastest_func.py
+++ 3,6_2_func-get_the_fastest_func.py
@@ -1,4 +1,4 @@
-'''
+"""
 Функция get_the_fastest_func()
 
 Реализуйте функцию get_the_fastest_func(), которая принимает два аргумента в следующем порядке:
@@ -7,8 +7,17 @@ funcs — список произвольных функций
 arg — произвольный объект
 Функция get_the_fastest_func() должна возвращать функцию из списка funcs, которая затратила 
 на вычисление значения при вызове с аргументом arg наименьшее количество времени.
-'''
+"""
+
 import time
 
+
 def get_the_fastest_func(func, arg):
-    pass
+    data = {}
+    for f in func:
+        start = time.time()
+        f(arg)
+        end = time.time()
+        res = end - start
+        data[f] = res
+    return min(data, key=data.get)
