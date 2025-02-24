686fc2b06f1638d5ab5101ba5c8d7617f9233928
Лев Романович Панас <jieba2005@list.ru>
2025-02-24 19:51:24 +0300

    week + calendar v.1

--- week.py
+++ week.py
@@ -6,20 +6,45 @@
 
 import locale
 from datetime import date, timedelta, datetime
+import calendar
 
-locale.setlocale(locale.LC_TIME, "ru_RU.UTF-8")
+locale.setlocale(locale.LC_ALL, "ru_RU.UTF-8")
 d, m, y = [int(i) for i in input("Введите дату в формате дд,мм,гггг: ").split(".")]
 dt = date(y, m, d)
 
 print("\n" f"Введена дата: {datetime.strftime(dt, '%x')}")
 print(f"День недели: {datetime.strftime(dt, '%A')}")
-print("\n" "Неделя, в которой есть данная дата: ")
 
 while dt.weekday() != 0:
     dt -= timedelta(days=1)
 
+print("\n" "Неделя, в которой есть данная дата: ")
 for i in range(7):
     print(datetime.strftime(dt, "%d %B %Y - %A"))
     dt += timedelta(days=1)
-    if dt.weekday() == 0:
-        break
+
+
+try:
+    while True:
+        s = input("Нужен ли календарь на месяц? y/n: ").lower()
+        if s in "y, n":
+            if s == "y":
+                print("\n", calendar.month(dt.year, dt.month))
+            break
+        else:
+            print("Пожалуйста, введите 'y' или 'n'.")
+except Exception as e:
+    print(f"Произошла ошибка: {e}")
+
+
+try:
+    while True:
+        s = input("Нужен ли календарь на год? y/n: ").lower()
+        if s in "y, n":
+            if s == "y":
+                print("\n", calendar.calendar(dt.year, m=4))
+            break
+        else:
+            print("Пожалуйста, введите 'y' или 'n'.")
+except Exception as e:
+    print(f"Произошла ошибка: {e}")
