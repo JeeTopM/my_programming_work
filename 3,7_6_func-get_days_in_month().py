157dc4673a439ff83bdf166f3e0b63b559c7e0aa
Лев Романович Панас <jieba2005@list.ru>
2025-02-26 00:18:51 +0300

    get_days_in_month()

---
+++ 3,7_6_func-get_days_in_month().py
@@ -0,0 +1,19 @@
+"""
+Функция get_days_in_month()   
+
+Реализуйте функцию get_days_in_month(), которая принимает два аргумента в следующем порядке:
+
+year — натуральное число
+month — полное название месяца на английском
+Функция должна возвращать отсортированный по возрастанию список всех дат (тип date) месяца month и года year.
+"""
+
+import calendar
+from datetime import datetime
+
+
+def get_days_in_month(year, month):
+    return
+
+
+get_days_in_month(2021, "December")
