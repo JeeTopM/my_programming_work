84878364777ca22c1f38393f65b171e157685938
Лев Романович Панас <jieba2005@list.ru>
2025-02-26 15:47:50 +0300

    add 3,7_7_func-get_all_mondays

---
+++ 3,7_7_func-get_all_mondays.py
@@ -0,0 +1,18 @@
+"""
+Функция get_all_mondays()
+
+Реализуйте функцию get_all_mondays(), которая принимает один аргумент:
+
+year — натуральное число
+Функция должна возвращать отсортированный по возрастанию список всех дат (тип date) года year, выпадающих на понедельник.
+"""
+
+import calendar
+from datetime import timedelta, date
+
+
+def get_all_mondays(year):
+    return
+
+
+print(get_all_mondays(2021))
