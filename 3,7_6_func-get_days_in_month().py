f1aa5db288595467734360f073459be06f655443
Лев Романович Панас <jieba2005@list.ru>
2025-02-26 13:15:42 +0300

    3,7_6_func-get_days_in_month() ver.1

--- 3,7_6_func-get_days_in_month().py
+++ 3,7_6_func-get_days_in_month().py
@@ -9,11 +9,32 @@ month — полное название месяца на английском
 """
 
 import calendar
-from datetime import datetime, date
+from datetime import timedelta, date
 
 
 def get_days_in_month(year, month):
-    return
+    list_month = list(calendar.month_name)
+    data_month = {}
+    for i in range(len(list_month)):
+        data_month[list_month[i]] = i
+    days = calendar.monthrange(year, data_month[month])
+    days_in_month = days[1]
+    start_day = date(year, data_month[month], 1)
+    all_days = []
+    for i in range(days_in_month):
+        all_days.append(start_day)
+        start_day += timedelta(days=1)
+    return all_days
 
 
-get_days_in_month(2021, "December")
+# Авторы
+"""
+def get_days_in_month(year, month):
+    month = list(calendar.month_name).index(month)
+    return [date(year, month, day) for day in range(1, calendar.monthrange(year, month)[1] + 1)]
+"""
+
+# Тест
+print(get_days_in_month(2021, "December"))
+year, month = input().split()
+print(get_days_in_month(int(year), month))
